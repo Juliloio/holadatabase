0ab378d519f4804baccc2057b7ca0e67650809aa
Juliloio <67295501+Juliloio@users.noreply.github.com>
2020-08-21 18:07:27 -0300

    Update user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const User = require('../../models/Cliente');
+const User = require('../../models/User');
 
 router.get('/clientes', (req, res) => {
   User.find((err, users) => {
