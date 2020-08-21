5c710ad1e04a5b3c8888f6048382d90d546d3c9b
Juliloio <julianaloiotile@gmail.com>
2020-08-21 17:33:09 -0300

    primer commit

--- models/User.js
+++ models/User.js
@@ -1,14 +1,12 @@
-// necesitamos importar mongoose
 const mongoose = require('mongoose');
-// los modelos se crean a partir de un schema
+
 const UserSchema = new mongoose.Schema({
-id: Number,
-name: String,
-mail: String,
-birthday: Date
+  id: Number,
+  nombre: String,
+  compras: [String],
+  precio: Number
 });
-// el schema describe la pinta de un documento de la coleccion
-// creamos el modelo llamando a mongoose.model(nombre, schema)
-const User = mongoose.model('User', UserSchema);
-// hay que exportar el modelo para usarlo despues en otros archivos
+
+const User = mongoose.model('Cliente', UserSchema);
+
 module.exports = User;
