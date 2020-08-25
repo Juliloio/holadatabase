442a656be91aae9c1f8ee7b437cb79a52b08a82e
Juliloio <67295501+Juliloio@users.noreply.github.com>
2020-08-25 13:55:38 -0300

    Update user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 const User = require('../../models/User');
 
-router.get('/users', (req, res) => {
+router.get('/User', (req, res) => {
   User.find((err, users) => {
     if (err) throw err;
     res.status(200).json(users);
     });
 });
 
-router.get('/users/:id', (req, res) => {
+router.get('/User/:id', (req, res) => {
   User.findOne({ id: req.params.id }, (err, user) => {
     if (err) throw err;
     res.status(200).json(user);
